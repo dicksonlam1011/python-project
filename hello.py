b8c24743a6ecbb20a2115ac6ce39f65116334490
Dickson LAM <LLM234@corp.ha.org.hk>
2021-09-03 17:28:59 +0800

    updated

--- hello.py
+++ hello.py
@@ -10,6 +10,8 @@ print("Start Python Project!")
 
 ppm_summary = pd.ExcelFile('python-project-source/promotion_summary_2021_Backup.xls')
 bi_weekly=pd.read_excel(ppm_summary, 'Bi-weekly')
+urgent = pd.read_excel(ppm_summary, 'Urgent')
+service = pd.read_excel(ppm_summary, 'Service')
 
 ####################################################################################
 filter_last_BW=input("Enter the last Release Day in YYYY-MM-DD format: ")
@@ -22,6 +24,17 @@ filter_next_BW=datetime.datetime(year2, month2, day2)
 
 BW_releaseSch=input("Enter the BW Release Schedule in XXXX-MM formart: ")
 ####################################################################################
+
+
+
+filter_datetime=urgent["Ready for Promotion"].apply(type)==datetime.datetime
+condition= (pd.isna(urgent["Serial no."])==False) & (urgent[filter_datetime]["Ready for Promotion"]>=filter_last_BW) & (urgent[filter_datetime]["Ready for Promotion"]<filter_next_BW) 
+print(urgent[condition]["Ready for Promotion"])
+
+
+# print(urgent["Ready for Promotion"])
+
+# print(bi_weekly["Ready for Promotion"])
 
 # datetime_list=[]
 # for col in bi_weekly["Ready for Promotion"].items():
@@ -36,7 +49,7 @@ BW_releaseSch=input("Enter the BW Release Schedule in XXXX-MM formart: ")
 
 # condition= (pd.isna(bi_weekly["Serial no."])==False) & (bi_weekly["Ready for Promotion"].apply(type)==datetime.datetime) & (bi_weekly["Ready for Promotion"]>=filter_last_BW) & (bi_weekly["Ready for Promotion"]<filter_next_BW) 
 
-pd.to_datetime(bi_weekly["Ready for Promotion"],errors="coerce")
+# pd.to_datetime(bi_weekly["Ready for Promotion"],errors="coerce")
 # print(bi_weekly["Ready for Promotion"])
 # filter_datetime=bi_weekly["Ready for Promotion"].apply(type)==datetime.datetime
 # condition= (pd.isna(bi_weekly["Serial no."])==False) & (bi_weekly["Ready for Promotion"]>=filter_last_BW) 
@@ -54,12 +67,12 @@ pd.to_datetime(bi_weekly["Ready for Promotion"],errors="coerce")
 # print(filter_datetime)
 # condition=(filter_datetime>=filter_last_BW) & (filter_datetime<filter_next_BW) & (pd.isna(bi_weekly["Serial no."])==False) 
 
-filter_data=bi_weekly["Ready for Promotion"].apply(type)==datetime.datetime
-# print(bi_weekly[filter_data])
+# filter_data=bi_weekly["Ready for Promotion"].apply(type)==datetime.datetime
+# # print(bi_weekly[filter_data])
 
-condition = (pd.isna(bi_weekly["Serial no."])==False) & (bi_weekly[filter_data]["Ready for Promotion"]>=filter_last_BW) & (bi_weekly[filter_data]["Ready for Promotion"]<filter_next_BW)
+# condition = (pd.isna(bi_weekly["Serial no."])==False) & (bi_weekly[filter_data]["Ready for Promotion"]>=filter_last_BW) & (bi_weekly[filter_data]["Ready for Promotion"]<filter_next_BW)
 
-print(bi_weekly[condition])
+# print(bi_weekly[condition])
 
 # print(bi_weekly["Ready for Promotion"])
 # condition=(pd.isna(bi_weekly["Serial no."])==False) & (BW_datetime[BW_datetime.apply(lambda x: isinstance(x, datetime.datetime))])
