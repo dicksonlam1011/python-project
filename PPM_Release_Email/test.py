d42a7429cdc5cd2eead624f4d4b5b579d3382d8c
dicksonlam1011 <36179324+dicksonlam1011@users.noreply.github.com>
2021-10-06 11:06:20 +0800

    updated

--- PPM_Release_Email/test.py
+++ PPM_Release_Email/test.py
@@ -1,13 +1,11 @@
 import win32com.client
 import pandas as pd
 
+# HA Source Path
+project_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM/Statistics_and_minutes/PPM_Statistics/2021"
+project_source_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM/Statistics_and_minutes/PPM_Statistics/2021/source"
 # Specify the Summary Year
 summary_year=input("Enter the summary year in YYYY format: ")
-
-# HA Source Path
-project_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM/Statistics_and_minutes/PPM_Statistics/{}".format(summary_year)
-project_source_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM/Statistics_and_minutes/PPM_Statistics/{}/source".format(summary_year)
-
 # Get sample source from local file
 ppm_summary = pd.ExcelFile('{}/promotion_summary_{}_Backup.xls'.format(project_source_path, summary_year))
 ### Part 2: Specify the Sheet Tab
@@ -20,7 +18,7 @@ elif promotion_type == "2":
     promotion_type=="Urgent"
     urgent = pd.read_excel(ppm_summary, 'Urgent')
 elif promotion_type == "3":
-    promotion_type="Service"
+    promotion_type=="Service"
     service = pd.read_excel(ppm_summary, 'Service')
 
 
@@ -39,18 +37,18 @@ remarks=service[condition]["Remarks"].item()
 
 
 # initiate the outlook application
-outlook = win32com.client.Dispatch('outlook.application')
+# outlook = win32com.client.Dispatch('outlook.application')
 
-# create an email object
-mail = outlook.CreateItem(0)
+# # create an email object
+# mail = outlook.CreateItem(0)
 
-# define email information
-mail.To = 'LLM234@ha.org.hk'
-mail.Subject = "[draft] CMS {} Release for {} - AAT".format(promotion_type, PPM_num)
+# # define email information
+# mail.To = 'LLM234@ha.org.hk'
+# mail.Subject = "[draft] CMS {} Release for {} - AAT".format(promotion_type, PPM_num)
 
-with open('C:/Users/llm234/Desktop/Development-Workplace/python-project/PPM_Release_Email/html_source/draft-e-form-email.html', 'r', encoding="utf-8") as myfile:
-    data=myfile.read().format(PPM_Number=PPM_num, Description=description, Target_date=target_date, Attachment=attachment, year=summary_year)
-mail.HTMLBody = data
+# with open('C:/Users/llm234/Desktop/Development-Workplace/python-project/PPM_Release_Email/html_source/draft-release-email.html', 'r', encoding="utf-8") as myfile:
+#     data=myfile.read().format(PPM_Number=PPM_num, Description=service[condition]["Description"], Target_date=service[condition]["Target Date"], Attachment=service[condition]["Attachment"], year=summary_year)
+# mail.HTMLBody = data
 
 # mail.HTMLBody = 'C:/Users/llm234/Desktop/Development-Workplace/python-project/PPM_Release_Email/html_source/draft-release-email.html'
 # mail.Body = "Testing Body"
@@ -59,4 +57,4 @@ mail.HTMLBody = data
 # mail.CC = 'somebody@company.com'
 
 # Send the email
-mail.Send()
+# mail.Send()
