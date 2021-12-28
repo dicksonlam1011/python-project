7e7a7182faab1e99bb37e11699a5eb5aea7192d6
dicksonlam1011 <36179324+dicksonlam1011@users.noreply.github.com>
2021-12-28 18:29:01 +0800

    updated on 28-Dec-2021

--- Code_Scanner/sybase_code_scanner.py
+++ Code_Scanner/sybase_code_scanner.py
@@ -6,6 +6,7 @@ ppm_sharefolder_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM"
 # month_path=input("Please enter the PPM Month folder (eg: 2021_12): \n")
 # ppm_folder=input("Please enter the PPM Folder Name (eg: 2021-S0492(K2)): \n")
 month_path="2021_12"
+# ppm_folder="Dickson_test"
 ppm_folder="2021-U0313(K2)"
 mypath="{}/{}/{}".format(ppm_sharefolder_path, month_path, ppm_folder)
 
@@ -17,13 +18,14 @@ def getAllSqlFileList():
                 sql_file_list.append(os.path.join(dirpath, x))
     return sql_file_list
 
-def getAllSqlNameList():
-    sql_filename_list = []
-    for (dirpath, subdirs, files) in os.walk(mypath):
-        for x in files:
-            if x.endswith((".sql",".ppm",".aat",".pps",".prd")):
-                sql_filename_list.append(x)
-    return sql_filename_list
+
+# def getAllSqlNameList():
+#     sql_filename_list = []
+#     for (dirpath, subdirs, files) in os.walk(mypath):
+#         for x in files:
+#             if x.endswith((".sql",".ppm",".aat",".pps",".prd")):
+#                 sql_filename_list.append(x)
+#     return sql_filename_list
 
 def getSpecificTypeSqlFilelist(script_type):
     sql_file_list = []
@@ -34,42 +36,49 @@ def getSpecificTypeSqlFilelist(script_type):
                 sql_file_list.append(os.path.join(dirpath, x))
     return sql_file_list
 
-def getSpecificTypeSqlNameList(script_type):
-    sql_filename_list = []
-    for (dirpath, subdirs, files) in os.walk(mypath):
-        for x in files:
-            if x.endswith((".sql",".ppm",".aat",".pps",".prd")) and (script_type in dirpath):
-                sql_filename_list.append(x)
-    return sql_filename_list
+# def getSpecificTypeSqlNameList(script_type):
+#     sql_filename_list = []
+#     for (dirpath, subdirs, files) in os.walk(mypath):
+#         for x in files:
+#             if x.endswith((".sql",".ppm",".aat",".pps",".prd")) and (script_type in dirpath):
+#                 sql_filename_list.append(x)
+#     return sql_filename_list
+
+def getFileNameOnly(sql_file_list):
+    # get filename only
+    filename=[]
+    for sql_file in sql_file_list:
+        filename.append(sql_file.split("\\")[-1])
+    return filename
 
 # Function 1: check header
-def checkSybaseHeader(sql_file_list, sql_filename_list):
-    checkHeaderResult=[]
-    for sql_file in sql_file_list:
-        for header_to_search in sql_filename_list:
-            with open(sql_file, 'r', encoding="utf-8") as file:
-                file = file.read()
-            if header_to_search in file:
-                checkHeaderResult.append("{}: Header Pass".format(header_to_search))
-            else:
-                checkHeaderResult.append("{}: Header Failed !!!".format(header_to_search))
-        break
-    return checkHeaderResult
+def checkHeaderInTheScript(content):
+    filename_list=getFileNameOnly(getAllSqlFileList())
+    for filename in filename_list:
+        if filename in content:
+            return filename
 
+def checkSybaseHeader(sql_file_list):
+    HeaderMatchedList=[]
+    for sql_file in sql_file_list:
+        with open(sql_file, 'r', encoding="utf-8") as file:
+            file = file.read()
+            HeaderMatchedList.append(checkHeaderInTheScript(file))
+
+    filename_list=getFileNameOnly(getAllSqlFileList())
+    # print(filename_list)
+    # print(HeaderMatchedList)
+    return set(filename_list).difference(HeaderMatchedList)
 
 # Function 2: check basic syntax
-def checkSybaseBasicSyntax(sql_file_list, sql_filename_list, string_to_search_1, string_to_search_2):
+def checkSybaseBasicSyntax(sql_file_list, string_to_search):
     checkBasicSyntaxResult=[]
     for sql_file in sql_file_list:
-        for sql_filename in sql_filename_list:
-            with open(sql_file, 'r', encoding="utf-8") as file:
-                file = file.read()
-                if (string_to_search_1 in file) or (string_to_search_2 in file):
-                    checkBasicSyntaxResult.append("{}: Basic Syntax Pass".format(sql_filename))
-                else:
-                    checkBasicSyntaxResult.append("{}: Basic Syntax Failed !!!".format(sql_filename))
-        break
-    return checkBasicSyntaxResult
+        with open(sql_file, 'r', encoding="utf-8") as file:
+            file = file.read()
+            if (string_to_search.lower() not in file) and (string_to_search.upper() not in file):
+                checkBasicSyntaxResult.append(sql_file)
+    return getFileNameOnly(checkBasicSyntaxResult)
 
 # Function 3: check db and table match
 # def checkSybaseDBandTable(sql_file_list, use_db, table):
@@ -82,63 +91,67 @@ def checkSybaseBasicSyntax(sql_file_list, sql_filename_list, string_to_search_1,
 #                 print("{}: DB & Table Structure Failed !!!".format(sql_file))
 
 # Function 4: check hospcode
-def checkSybaseHospcode(manual_sql_file_list, manual_sql_filename_list, hospcode):
+def checkSybaseHospcode(sql_file_list, hospcode):
     checkHospcodeResult=[]
-    for sql_file in manual_sql_file_list:
-        for manual_sql_filename in manual_sql_filename_list:
-            with open(sql_file, 'r', encoding="utf-8") as file:
-                file = file.read()
-                # if (script_type in sql_file) and (hospcode in file):
-                if hospcode in file:
-                    checkHospcodeResult.append("{}: Hospcode Match Pass".format(manual_sql_filename))
-                else:
-                    checkHospcodeResult.append("{}: Hospcode Match Failed !!!".format(manual_sql_filename))
-        break
-    return checkHospcodeResult
+    for sql_file in sql_file_list:
+        # for sql_filename in sql_filename_list:
+        with open(sql_file, 'r', encoding="utf-8") as file:
+            file = file.read()
+            # if (script_type in sql_file) and (hospcode in file):
+            if hospcode in file:
+                checkHospcodeResult.append(sql_file)
+
+    return getFileNameOnly(checkHospcodeResult)
 
 def OutputResult():
-    header_result=checkSybaseHeader(getAllSqlFileList(), getAllSqlNameList())
-    basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go", "GO")
-    corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_manual_"), getSpecificTypeSqlNameList("_manual_"),"HAH")
-    hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-manual_"), getSpecificTypeSqlNameList("_imp-manual_"), "hospcode")
-
-    with open(os.path.join(mypath,"{}-header-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in header_result:
-            file.write(result+"\n")
-
-    with open(os.path.join(mypath,"{}-syntax-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in basic_syntax_result:
-            file.write(result+"\n") 
-    
-    with open(os.path.join(mypath,"{}-corp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in corp_hospcode_result:
-            file.write(result+"\n") 
-    
-    with open(os.path.join(mypath,"{}-hosp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in hosp_hospcode_result:
-            file.write(result+"\n") 
-    
-
-
-    # file = open(os.path.join(mypath,"{}-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8")
+    # check header match
     # header_result=checkSybaseHeader(getAllSqlFileList(), getAllSqlNameList())
-    # basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go", "GO")
-    # corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_manual_"), getSpecificTypeSqlNameList("_manual_"),"HAH")
-    # hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-manual_"), getSpecificTypeSqlNameList("_imp-manual_"), "hospcode")
+    # # check syntax "go"
+    # basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go")
+    # # check script type (fail case)
+    # corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"), getSpecificTypeSqlNameList("_corp-db_"), "##hospcode##")
+    # hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"), getSpecificTypeSqlNameList("_imp-corp-db_"), "HAH")
 
-    # for result in header_result:
-    #     file.write(result+"\n")
+    # with open(os.path.join(mypath,"{}-header-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in header_result:
+    #         file.write(result+"\n")
 
-    # for result in basic_syntax_result:
-    #     file.write(result+"\n")
+    # with open(os.path.join(mypath,"{}-syntax-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in basic_syntax_result:
+    #         file.write(result+"\n") 
+    
+    # with open(os.path.join(mypath,"{}-corp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in corp_hospcode_result:
+    #         file.write(result+"\n") 
+    
+    # with open(os.path.join(mypath,"{}-hosp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in hosp_hospcode_result:
+    #         file.write(result+"\n") 
+    
 
-    # for result in corp_hospcode_result:
-    #     file.write(result+"\n")
+    file = open(os.path.join(mypath,"{}-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8")
+    header_result=checkSybaseHeader(getAllSqlFileList())
+    basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(),"go")
+    corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"),"##hospcode##")
+    hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"),"HAH" )
 
-    # for result in hosp_hospcode_result:
-    #     file.write(result+"\n")
+    file.write("=================Header Scanning Result====================="+"\n")
+    for result in header_result:
+        file.write(result+": Header Result Failed!! "+"\n")
 
-    # file.close()
+    file.write("=================Syntax Scanning Result====================="+"\n")
+    for result in basic_syntax_result:
+        file.write(result+": Go is missing in the script !! "+"\n")
+
+    file.write("=================Central script variable Scanning Result====================="+"\n") 
+    for result in corp_hospcode_result:
+        file.write(result+": Central script variable mistake!! "+"\n")
+
+    file.write("=================Hospital script variable Scanning Result====================="+"\n") 
+    for result in hosp_hospcode_result:
+        file.write(result+": Hospital script variable mistake!! "+"\n")
+
+    file.close()
 
 OutputResult()
         
--- Code_Scanner/test.py
+++ Code_Scanner/test.py
@@ -6,7 +6,8 @@ ppm_sharefolder_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM"
 # month_path=input("Please enter the PPM Month folder (eg: 2021_12): \n")
 # ppm_folder=input("Please enter the PPM Folder Name (eg: 2021-S0492(K2)): \n")
 month_path="2021_12"
-ppm_folder="Dickson_test"
+# ppm_folder="Dickson_test"
+ppm_folder="2021-U0313(K2)"
 mypath="{}/{}/{}".format(ppm_sharefolder_path, month_path, ppm_folder)
 
 def getAllSqlFileList():
@@ -18,13 +19,13 @@ def getAllSqlFileList():
     return sql_file_list
 
 
-def getAllSqlNameList():
-    sql_filename_list = []
-    for (dirpath, subdirs, files) in os.walk(mypath):
-        for x in files:
-            if x.endswith((".sql",".ppm",".aat",".pps",".prd")):
-                sql_filename_list.append(x)
-    return sql_filename_list
+# def getAllSqlNameList():
+#     sql_filename_list = []
+#     for (dirpath, subdirs, files) in os.walk(mypath):
+#         for x in files:
+#             if x.endswith((".sql",".ppm",".aat",".pps",".prd")):
+#                 sql_filename_list.append(x)
+#     return sql_filename_list
 
 def getSpecificTypeSqlFilelist(script_type):
     sql_file_list = []
@@ -35,43 +36,49 @@ def getSpecificTypeSqlFilelist(script_type):
                 sql_file_list.append(os.path.join(dirpath, x))
     return sql_file_list
 
-def getSpecificTypeSqlNameList(script_type):
-    sql_filename_list = []
-    for (dirpath, subdirs, files) in os.walk(mypath):
-        for x in files:
-            if x.endswith((".sql",".ppm",".aat",".pps",".prd")) and (script_type in dirpath):
-                sql_filename_list.append(x)
-    return sql_filename_list
+# def getSpecificTypeSqlNameList(script_type):
+#     sql_filename_list = []
+#     for (dirpath, subdirs, files) in os.walk(mypath):
+#         for x in files:
+#             if x.endswith((".sql",".ppm",".aat",".pps",".prd")) and (script_type in dirpath):
+#                 sql_filename_list.append(x)
+#     return sql_filename_list
+
+def getFileNameOnly(sql_file_list):
+    # get filename only
+    filename=[]
+    for sql_file in sql_file_list:
+        filename.append(sql_file.split("\\")[-1])
+    return filename
 
 # Function 1: check header
-def checkSybaseHeader(sql_file_list, sql_filename_list):
-    checkHeaderResult=[]
-    for sql_file in sql_file_list:
-        for header_to_search in sql_filename_list:
-            with open(sql_file, 'r', encoding="utf-8") as file:
-                file = file.read()
-            if header_to_search not in file:
-            #     checkHeaderResult.append("{}: Header Pass".format(header_to_search))
-            # else:
-                checkHeaderResult.append("{}: Header Failed !!!".format(header_to_search))
-        break
-    return checkHeaderResult
+def checkHeaderInTheScript(content):
+    filename_list=getFileNameOnly(getAllSqlFileList())
+    for filename in filename_list:
+        if filename in content:
+            return filename
 
+def checkSybaseHeader(sql_file_list):
+    HeaderMatchedList=[]
+    for sql_file in sql_file_list:
+        with open(sql_file, 'r', encoding="utf-8") as file:
+            file = file.read()
+            HeaderMatchedList.append(checkHeaderInTheScript(file))
+
+    filename_list=getFileNameOnly(getAllSqlFileList())
+    # print(filename_list)
+    # print(HeaderMatchedList)
+    return set(filename_list).difference(HeaderMatchedList)
 
 # Function 2: check basic syntax
-def checkSybaseBasicSyntax(sql_file_list, sql_filename_list, string_to_search):
+def checkSybaseBasicSyntax(sql_file_list, string_to_search):
     checkBasicSyntaxResult=[]
     for sql_file in sql_file_list:
-        for sql_filename in sql_filename_list:
-            with open(sql_file, 'r', encoding="utf-8") as file:
-                file = file.read()
-                if (string_to_search not in file) and (string_to_search.upper() not in file):
-                #     checkBasicSyntaxResult.append("{}: Basic Syntax Pass".format(sql_filename))
-                # else:
-                    checkBasicSyntaxResult.append("{}: Basic Syntax Failed !!!".format(sql_filename))
-        break
-    return checkBasicSyntaxResult
-
+        with open(sql_file, 'r', encoding="utf-8") as file:
+            file = file.read()
+            if (string_to_search.lower() not in file) and (string_to_search.upper() not in file):
+                checkBasicSyntaxResult.append(sql_file)
+    return getFileNameOnly(checkBasicSyntaxResult)
 
 # Function 3: check db and table match
 # def checkSybaseDBandTable(sql_file_list, use_db, table):
@@ -93,63 +100,60 @@ def checkSybaseHospcode(sql_file_list, hospcode):
             # if (script_type in sql_file) and (hospcode in file):
             if hospcode in file:
                 checkHospcodeResult.append(sql_file)
-    filename=[]
-    for sql_file in checkHospcodeResult:
-        filename.append(sql_file.split("\\")[4])
-        # break
-    return filename
 
-# print(checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"), getSpecificTypeSqlNameList("_imp-corp-db_"), "HAH"))
-print(checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"), "HAH"))
+    return getFileNameOnly(checkHospcodeResult)
 
 def OutputResult():
     # check header match
-    header_result=checkSybaseHeader(getAllSqlFileList(), getAllSqlNameList())
-    # check syntax "go"
-    basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go")
-    # check script type (fail case)
-    corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"), getSpecificTypeSqlNameList("_corp-db_"), "##hospcode##")
-    hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"), getSpecificTypeSqlNameList("_imp-corp-db_"), "HAH")
-
-    with open(os.path.join(mypath,"{}-header-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in header_result:
-            file.write(result+"\n")
-
-    with open(os.path.join(mypath,"{}-syntax-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in basic_syntax_result:
-            file.write(result+"\n") 
-    
-    with open(os.path.join(mypath,"{}-corp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in corp_hospcode_result:
-            file.write(result+"\n") 
-    
-    with open(os.path.join(mypath,"{}-hosp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
-        for result in hosp_hospcode_result:
-            file.write(result+"\n") 
-    
-
-
-    # file = open(os.path.join(mypath,"{}-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8")
     # header_result=checkSybaseHeader(getAllSqlFileList(), getAllSqlNameList())
-    # basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go", "GO")
-    # corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_manual_"), getSpecificTypeSqlNameList("_manual_"),"HAH")
-    # hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-manual_"), getSpecificTypeSqlNameList("_imp-manual_"), "hospcode")
+    # # check syntax "go"
+    # basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(), getAllSqlNameList(), "go")
+    # # check script type (fail case)
+    # corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"), getSpecificTypeSqlNameList("_corp-db_"), "##hospcode##")
+    # hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"), getSpecificTypeSqlNameList("_imp-corp-db_"), "HAH")
 
-    # for result in header_result:
-    #     file.write(result+"\n")
+    # with open(os.path.join(mypath,"{}-header-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in header_result:
+    #         file.write(result+"\n")
 
-    # for result in basic_syntax_result:
-    #     file.write(result+"\n")
+    # with open(os.path.join(mypath,"{}-syntax-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in basic_syntax_result:
+    #         file.write(result+"\n") 
+    
+    # with open(os.path.join(mypath,"{}-corp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in corp_hospcode_result:
+    #         file.write(result+"\n") 
+    
+    # with open(os.path.join(mypath,"{}-hosp-hospcode-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8") as file:
+    #     for result in hosp_hospcode_result:
+    #         file.write(result+"\n") 
+    
 
-    # for result in corp_hospcode_result:
-    #     file.write(result+"\n")
+    file = open(os.path.join(mypath,"{}-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8")
+    header_result=checkSybaseHeader(getAllSqlFileList())
+    basic_syntax_result=checkSybaseBasicSyntax(getAllSqlFileList(),"go")
+    corp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"),"##hospcode##")
+    hosp_hospcode_result=checkSybaseHospcode(getSpecificTypeSqlFilelist("_imp-corp-db_"),"HAH" )
 
-    # for result in hosp_hospcode_result:
-    #     file.write(result+"\n")
+    file.write("=================Header Scanning Result====================="+"\n")
+    for result in header_result:
+        file.write(result+": Header Result Failed!! "+"\n")
 
-    # file.close()
+    file.write("=================Syntax Scanning Result====================="+"\n")
+    for result in basic_syntax_result:
+        file.write(result+": Go is missing in the script !! "+"\n")
 
-# OutputResult()
+    file.write("=================Central script variable Scanning Result====================="+"\n") 
+    for result in corp_hospcode_result:
+        file.write(result+": Central script variable mistake!! "+"\n")
+
+    file.write("=================Hospital script variable Scanning Result====================="+"\n") 
+    for result in hosp_hospcode_result:
+        file.write(result+": Hospital script variable mistake!! "+"\n")
+
+    file.close()
+
+OutputResult()
         
 
 # def runCodeScanner():
