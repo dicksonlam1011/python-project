25366df0587670178e0a25fc9693ecf4709f6f6e
dicksonlam1011 <36179324+dicksonlam1011@users.noreply.github.com>
2021-12-31 17:45:54 +0800

    finished & please test

--- Code_Scanner/sybase_code_scanner.py
+++ Code_Scanner/sybase_code_scanner.py
@@ -2,13 +2,13 @@ import datetime
 import os
 import operator
 
-ppm_sharefolder_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM"
-# month_path=input("Please enter the PPM Month folder (eg: 2021_12): \n")
-# ppm_folder=input("Please enter the PPM Folder Name (eg: 2021-S0492(K2)): \n")
-month_path="2021_12"
-ppm_folder="Dickson_test"
-# ppm_folder=""
-mypath="{}/{}/{}".format(ppm_sharefolder_path, month_path, ppm_folder)
+# ppm_sharefolder_path="//dc7shdns02b/CSC1/TEAMFOLDER/PPM"
+# # month_path=input("Please enter the PPM Month folder (eg: 2021_12): \n")
+# # ppm_folder=input("Please enter the PPM Folder Name (eg: 2021-S0492(K2)): \n")
+# month_path="2021_12"
+# ppm_folder="Dickson_test"
+# mypath="{}/{}/{}".format(ppm_sharefolder_path, month_path, ppm_folder)
+mypath=input("Plesae input the PPM source path:"+"\n")
 
 def getAllSqlFileList():
     sql_file_list = []
@@ -175,7 +175,7 @@ def OutputResult():
     #         file.write(result+"\n") 
     
 
-    file = open(os.path.join(mypath,"{}-checking_result.log".format(ppm_folder)), mode="w",encoding="utf8")
+    file = open(os.path.join(mypath,"scanning_result.log"), mode="w",encoding="utf8")
     header_scan=checkSybaseHeader(getAllSqlFileList())
     basic_syntax_scan=checkSybaseBasicSyntax(getAllSqlFileList(),"go")
     corp_hospcode_scan=checkSybaseHospcode(getSpecificTypeSqlFilelist("_corp-db_"),"##hospcode##")
@@ -187,52 +187,80 @@ def OutputResult():
     local_moe_manual_scan=checkFilePath(getAllSqlFileList(),"LOCAL_MOE","_manual_")
 
     file.write("Scanning 1: =================Header scanning result====================="+"\n")
-    file.write("==> The following script(s) Header Information (script name) does NOT matched with the source"+"\n")
-    for result in header_scan:
-        # file.write(result+": Header Result Failed!! "+"\n")
-        file.write(result+"\n")
+    if len(header_scan)>=1:
+        file.write("==> The following script(s) Header Information (script name) does NOT matched with the source"+"\n")
+        for result in header_scan:
+            # file.write(result+": Header Result Failed!! "+"\n")
+            file.write(result+"\n")
+    else:
+        file.write("Scanned script header information PASS"+"\n")
+
+
 
     file.write("\n"+"Scanning 2: =================Syntax scanning result====================="+"\n")
-    file.write("==> The following script(s) missing 'GO' in the scripts"+"\n")
-    for result in basic_syntax_scan:
-        # file.write(result+": Go is missing in the script !! "+"\n")
-        file.write(result+"\n")
+    if len(basic_syntax_scan)>=1:
+        file.write("==> The following script(s) missing 'GO' in the scripts"+"\n")
+        for result in basic_syntax_scan:
+                # file.write(result+": Go is missing in the script !! "+"\n")
+                file.write(result+"\n")
+    else:
+        file.write("Scanned script syntax information PASS"+"\n")
+
 
     file.write("\n"+"Scanning 3: =================Central/Hospital E-form variable scanning result====================="+"\n") 
-    file.write("==> The following script(s) occurred E-form variables mistables "+"\n")
-    for result in corp_hospcode_scan:
-        # file.write(result+": Central script variable mistake!! "+"\n")
-        file.write(result+"\n")
-    for result in hosp_hospcode_scan:
-        # file.write(result+": Hospital script variable mistake!! "+"\n")
-        file.write(result+"\n")
+    if len(corp_hospcode_scan)>=1:
+        file.write("==> The following script(s) occurred E-form variables mistables "+"\n")
+        for result in corp_hospcode_scan:
+            # file.write(result+": Central script variable mistake!! "+"\n")
+            file.write(result+"\n")
+        for result in hosp_hospcode_scan:
+            # file.write(result+": Hospital script variable mistake!! "+"\n")
+            file.write(result+"\n")
+    else:
+        file.write("Scanned script e-form variable PASS"+"\n")
 
 
     file.write("\n"+"Scanning 4: =================Corp 7 menu function list scanning result====================="+"\n") 
-    file.write("==> The following script(s) involve CORP7 menu function list, please check the E-form setup information for the step of refresh cache"+"\n")
-    for result in corp7_menu_function_list_scan:
-        # file.write(result+": Scanned presence of CORP7 menu function list script, please state refresh cache step in promotion form "+"\n")
-       file.write(result+"\n")
+    if len(corp7_menu_function_list_scan)>=1:
+        file.write("==> The following script(s) involve CORP7 menu function list, please check the E-form setup information for the step of refresh cache"+"\n")
+        for result in corp7_menu_function_list_scan:
+            # file.write(result+": Scanned presence of CORP7 menu function list script, please state refresh cache step in promotion form "+"\n")
+            file.write(result+"\n")
+    else:
+        file.write("Scanned No CORP7 menu function list included"+"\n")
+
 
     file.write("\n"+"Scanning 5: =================Alter scripts scanning result====================="+"\n") 
-    file.write("==> The following script(s) of Alter Tables are NOT set to be manual types"+"\n")
-    for result in alter_script_scan:
-        # file.write(result+": Scanned alter scripts are NOT set to be manual workflow"+"\n")
-        file.write(result+"\n")
+    if len(alter_script_scan)>=1:
+        file.write("==> The following script(s) of Alter Tables are NOT set to be manual types"+"\n")
+        for result in alter_script_scan:
+            # file.write(result+": Scanned alter scripts are NOT set to be manual workflow"+"\n")
+            file.write(result+"\n")
+    else:
+        file.write("Scanned no alter scripts set to be wrong source type"+"\n")
+
 
     file.write("\n"+"Scanning 6: =================Local MOE scripts scanning result====================="+"\n") 
-    file.write("==> The following local moe script(s) missing 'XXXmoe_db' in PPM"+"\n")
-    for result in local_moe_manual_content_scan:
-        file.write(result+": Scanned local moe imp-manual scripts no XXXmoe_db"+"\n")
+    if len(local_moe_manual_content_scan)>=1:
+        file.write("==> The following local moe script(s) missing 'XXXmoe_db' in PPM"+"\n")
+        for result in local_moe_manual_content_scan:
+            file.write(result+": Scanned local moe imp-manual scripts no XXXmoe_db"+"\n")
+    else:
+        file.write("Scanned manual local moe script PASS"+"\n")
+
 
     file.write("\n"+"Scanning 7: =================Manual types of LOE and Local MOE scripts scanning result====================="+"\n") 
-    file.write("==> The following local moe/LOE script(s) are set to wrong source type"+"\n")
-    for result in loe_manual_scan:
-        # file.write(result+": Scanned Loe/ local moe manual-workflow script wrong script type"+"\n")
-        file.write(result+"\n")
-    for result in local_moe_manual_scan:
-        # file.write(result+": Scanned Loe/ local moe manual-workflow script wrong script type"+"\n")
-        file.write(result+"\n")
+    if len(loe_manual_scan)>=1:
+        file.write("==> The following local moe/LOE script(s) are set to wrong source type"+"\n")
+        for result in loe_manual_scan:
+            # file.write(result+": Scanned Loe/ local moe manual-workflow script wrong script type"+"\n")
+            file.write(result+"\n")
+        for result in local_moe_manual_scan:
+            # file.write(result+": Scanned Loe/ local moe manual-workflow script wrong script type"+"\n")
+            file.write(result+"\n")
+    else:
+        file.write("Scanned loe/local moe script source type PASS"+"\n")
+
         
     file.close()
 
